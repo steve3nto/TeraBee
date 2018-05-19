cc5f11fd6a42622bbad6228f3ddc7eace7aac23c
Steve3nto <stefano.imoscopi@gmail.com>
2018-05-19 13:29:33 +0200

    Added Processing and SuperCollider

--- Code/parse_text_output.py
+++ Code/parse_text_output.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 data = []
-filepath = "C:/Users/Stefano/Desktop/MUSIC_HACK/Code/output_2018-05-19_09-15-29.log"
+filepath = "C:/Users/Stefano/Desktop/Terabee/Code/output_2018-05-19_09-59-22_raw.txt"
 
 with open(filepath, 'r') as file:
-    big_string = str(file.read())
-    data = big_string.split('MF')
-    print(data)
+    content = file.read()
+
+lines = content.split("\n")
+numstr = [line.split("\t")[1:] for line in lines if line != ""]
+numbers = np.array(numstr, dtype=np.int32)
+
+print(numbers)
 
     
     

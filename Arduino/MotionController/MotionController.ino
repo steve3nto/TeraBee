f20aa3f76543438cab3da102e973be4aa6ac2d1f
Steve3nto <stefano.imoscopi@gmail.com>
2018-09-21 09:22:42 +0200

    Working, but missing hold mode

--- Arduino/MotionController/MotionController.ino
+++ Arduino/MotionController/MotionController.ino
@@ -9,15 +9,19 @@
 const int num_sensors = 3;
 
 typedef struct {    // holds all free parameters (for presets)
-  int reverse[num_sensors];
-  int active[num_sensors];
+  bool reverse[num_sensors];
+  int activePreset;
+  int waitState; 
+  int rubberState; 
+  int activeState[num_sensors];   // state of the buttons in hold mode
+  int presetState[num_sensors];  // state of the buttons in normal mode
   int sensor_range[num_sensors];
   int cc_num[num_sensors];
   int channel[num_sensors];
   int default_value[num_sensors];  // for rubber-band mode
   int min_value[num_sensors];
   int max_value[num_sensors];
-} parameters;
+} parameters_t;
 
 // Created and binds the MIDI interface to the default hardware Serial port
 MIDI_CREATE_DEFAULT_INSTANCE();
@@ -26,16 +30,19 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 byte type, d1, d2;
 
 // struct that holds all current parameters
-parameters params;  
+parameters_t params;  
 
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin1 = 4;     // button for standby
 const int buttonPin2 = 5;     // button for elastic-mode
+const int buttonPin3 = 8;
+const int buttonPin4 = 9;
+const int buttonPin5 = 10;  // the buttons for the 3 presets / sensor activation
 const int ledPin1 =  6;       // rubber band mode LED (green)
 const int ledPin2 =  7;       // standby LED (red)
-const int buttonPin3 = 9;
-const int buttonPin4 = 10;
-const int buttonPin5 = 11;  // the buttons for the 3 presets / sensor activation
+const int ledPin3 =  11;       
+const int ledPin4 =  12;       
+const int ledPin5 =  13;
 
 int buttonState1 = HIGH;         // current state of the button (LOW = ON)
 int lastButtonState1 = HIGH;     // previous state of the button
@@ -47,11 +54,7 @@ int buttonState4 = HIGH;
 int lastButtonState4 = HIGH;
 int buttonState5 = HIGH;        
 int lastButtonState5 = HIGH;          
-int waitState = HIGH;
-int rubberState = HIGH;
-int presetState1 = HIGH;
-int presetState2 = HIGH;
-int presetState3 = HIGH;
+bool check1, check2, check3;  // to check if preset buttons have been pressed
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
@@ -95,6 +98,9 @@ void setup() {
   // initialize LED pins as outputs:
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
   // initialize the pushbuttons as inputs:
   pinMode(buttonPin1, INPUT_PULLUP);
   pinMode(buttonPin2, INPUT_PULLUP);
@@ -102,8 +108,23 @@ void setup() {
   pinMode(buttonPin4, INPUT_PULLUP);
   pinMode(buttonPin5, INPUT_PULLUP);
 
-  digitalWrite(ledPin1, rubberState);
-  digitalWrite(ledPin2, waitState);
+  // Load params from EEPROM ################# TO DO ###################
+  params.rubberState = HIGH;    // reversed logic
+  params.waitState = HIGH;
+  params.activeState[0] = LOW;  
+  params.activeState[1] = LOW;  // all active by default
+  params.activeState[2] = LOW;
+
+  // Choose preset 1, 2 or 3 
+  params.activePreset = 1;   
+  Upload_preset_state(); 
+  
+  // light leds according to params
+  digitalWrite(ledPin1, params.rubberState);
+  digitalWrite(ledPin2, params.waitState);
+  digitalWrite(ledPin3, params.presetState[0]);
+  digitalWrite(ledPin4, params.presetState[1]);
+  digitalWrite(ledPin5, params.presetState[2]);
  
   // initialize first_time and reverse flags
   for(int i=0; i<3; i++)
@@ -111,8 +132,7 @@ void setup() {
       first_time[i] = true;
   }
 
-  // Choose preset 1, 2 or 3 by changing the function name
-  initialise_preset1();  // initial parameters are hard-coded in the function definition
+
   delay(3000);
 
 }
@@ -121,8 +141,8 @@ void loop() {
   // Logic for the button presses
   //##########################################################
 
-  // Check if HOLD MODE gets activated
-  ButtonHold(buttonPin1, ledPin2, holdStart, holdTime, holdPressed);
+  // Check if HOLD MODE gets activated   ###### TO FIX ##########
+  // ButtonHold(buttonPin1, ledPin2, holdStart, holdTime, holdPressed);
   
   // read the state of the pushbutton value:
   int reading1 = digitalRead(buttonPin1);
@@ -132,30 +152,70 @@ void loop() {
   int reading5 = digitalRead(buttonPin5); 
 
   // WAIT BUTTON
-  buttonPress(reading1, lastButtonState1, waitState, ledPin2,
+  buttonPress(reading1, lastButtonState1, params.waitState, ledPin1,
                 buttonState1, lastDebounceTime1, debounceDelay1);
   
   // RUBBER BAND BUTTON
-  buttonPress(reading2, lastButtonState2,rubberState, ledPin1,
+  buttonPress(reading2, lastButtonState2, params.rubberState, ledPin2,
                 buttonState2, lastDebounceTime2, debounceDelay2);
   
   // PRESET BUTTON 1
-  buttonPress(reading3, lastButtonState3, presetState1, ledPin2,
+  check1 = buttonPress(reading3, lastButtonState3, params.presetState[0], ledPin3,
                 buttonState3, lastDebounceTime3, debounceDelay3);
-
+  if(check1)
+  {
+    initialise_preset1();
+    // OPTIONAL, FLASH ACTIVE SENSORS
+    digitalWrite(ledPin3, params.activeState[0]);
+    digitalWrite(ledPin4, params.activeState[1]);
+    digitalWrite(ledPin5, params.activeState[2]);
+    delay(500);
+    
+    digitalWrite(ledPin3, params.presetState[0]);
+    digitalWrite(ledPin4, params.presetState[1]);
+    digitalWrite(ledPin5, params.presetState[2]);
+  }
+  
   // PRESET BUTTON 2
-  buttonPress(reading4, lastButtonState4, presetState2, ledPin2,
+  check2 = buttonPress(reading4, lastButtonState4, params.presetState[1], ledPin4,
                 buttonState4, lastDebounceTime4, debounceDelay4);
+  if(check2)
+  {
+    initialise_preset2();
+
+    // OPTIONAL, FLASH ACTIVE SENSORS
+    digitalWrite(ledPin3, params.activeState[0]);
+    digitalWrite(ledPin4, params.activeState[1]);
+    digitalWrite(ledPin5, params.activeState[2]);
+    delay(500);
+    
+    digitalWrite(ledPin3, params.presetState[0]);
+    digitalWrite(ledPin4, params.presetState[1]);
+    digitalWrite(ledPin5, params.presetState[2]);
+  }
 
   // PRESET BUTTON 3
-  buttonPress(reading5, lastButtonState5, presetState3, ledPin2,
+  check3 = buttonPress(reading5, lastButtonState5, params.presetState[2], ledPin5,
                 buttonState5, lastDebounceTime5, debounceDelay5);
+  if(check3)
+  {
+    initialise_preset3();
+    // OPTIONAL, FLASH ACTIVE SENSORS
+    digitalWrite(ledPin3, params.activeState[0]);
+    digitalWrite(ledPin4, params.activeState[1]);
+    digitalWrite(ledPin5, params.activeState[2]);
+    delay(500);
+    
+    digitalWrite(ledPin3, params.presetState[0]);
+    digitalWrite(ledPin4, params.presetState[1]);
+    digitalWrite(ledPin5, params.presetState[2]);
+  }
 
   
   //#############################################################################//
   // MAIN CODE for each state (wait, rubber, normal mode)
   
-  if(waitState == LOW)  // low state means active
+  if(params.waitState == LOW)  // low state means active
   {
     return;  // waiting mode, controller is idle
   }
@@ -184,7 +244,7 @@ void loop() {
   }
   
 
-  if(rubberState == LOW)
+  if(params.rubberState == LOW)
   {
     sendRubberBand();
     mergeMIDI();          
@@ -226,6 +286,11 @@ void sendMIDIcc()
 {
   for(int i=0; i < num_sensors; i++)  // loop over sensors
   {
+      if(params.activeState[i] == HIGH)  // low means active
+      {
+        continue;  
+      }
+      
       if(data[i] != -256)  // -256 is infinite distance from sensor (no object detected)
       {
           // map data to MIDI CC range [0,127]
@@ -259,6 +324,12 @@ void sendRubberBand()
 {
   for(int i=0; i < num_sensors; i++)  // loop over sensors
   {
+      
+      if(params.activeState[i] == HIGH)  // low means active
+      {
+        continue;  
+      }
+      
       if(data[i] != -256)  // -256 is infinite distance from sensor (no hand detected)
       {
           // map data to MIDI CC range [0,127]
@@ -288,7 +359,7 @@ void sendRubberBand()
 }
 
 
-void buttonPress(int& reading, int& lastButtonState, int& state, int ledPin,
+bool buttonPress(int& reading, int& lastButtonState, int& state, int ledPin,
        int& buttonState,unsigned long& lastDebounceTime, unsigned long& debounceDelay)
 {
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
@@ -307,14 +378,17 @@ void buttonPress(int& reading, int& lastButtonState, int& state, int ledPin,
       // only toggle the LED if the new button state is HIGH (press and release)
       if (buttonState == HIGH) {
         state = !state;
+        // set the LED:
+        digitalWrite(ledPin, state);
+        lastButtonState = reading;
+        return true;  // returns true if state has changed
       }
     }    
-    
   }  
-  // set the LED:
-  digitalWrite(ledPin, state);
+
   // save the reading. Next time through the loop, it'll be the lastButtonState:
-  lastButtonState = reading;  
+  lastButtonState = reading;
+  return false;  
 }
 
 void ButtonHold(int buttonPin, int ledPin, unsigned long& holdStart, 
@@ -329,16 +403,33 @@ void ButtonHold(int buttonPin, int ledPin, unsigned long& holdStart,
   if((millis() - holdStart) > holdTime && holdPressed) {  // button has been held 
     holdMode(ledPin, buttonPin);  
   }
+
+  
 }
 
 void holdMode(int ledPin, int buttonPin)
 {
+    // switch leds to active sensors!
+    digitalWrite(ledPin3, params.activeState[0]);
+    digitalWrite(ledPin4, params.activeState[1]);
+    digitalWrite(ledPin5, params.activeState[2]);
+    
     while(digitalRead(buttonPin) == LOW)
     {
       digitalWrite(ledPin, !holdMode);
       delay(1000);
       digitalWrite(ledPin, holdMode);    
       delay(1000);
+
+      int r3 = digitalRead(buttonPin3);
+      int r4 = digitalRead(buttonPin4);
+      int r5 = digitalRead(buttonPin5); 
+      HoldModeButtons(r3, r4, r5);  
+      // switch leds to active sensors!
+      digitalWrite(ledPin3, params.activeState[0]);
+      digitalWrite(ledPin4, params.activeState[1]);
+      digitalWrite(ledPin5, params.activeState[2]);
+      
     }  
     holdPressed = false;
 }
@@ -346,28 +437,68 @@ void holdMode(int ledPin, int buttonPin)
 void initialise_preset1() {
     params.cc_num[0] = 22;  params.cc_num[1] = 23; params.cc_num[2] = 24;
     params.reverse[0] = false; params.reverse[1] = false; params.reverse[2] = false;
+    params.activeState[0] = LOW; params.activeState[1] = LOW; params.activeState[2] = LOW;
+    // (min, max) = (40, 150) sensor range due to terabee sensor characteristics
     params.sensor_range[0] = 150; params.sensor_range[1] = 150; params.sensor_range[2] = 150; 
     params.default_value[0] = 64; params.default_value[1] = 64; params.default_value[2] = 64;  
+    // lowest possible is 0, highest possible is 127
     params.min_value[0] = 0; params.min_value[1] = 0; params.min_value[2] = 0;
     params.max_value[0] = 127; params.max_value[1] = 127; params.max_value[2] = 127;
+    params.activePreset = 1;
+    params.presetState[0] = LOW; params.presetState[1] = HIGH; params.presetState[2] = HIGH;
 }
 
 void initialise_preset2() {
     params.cc_num[0] = 22;  params.cc_num[1] = 23; params.cc_num[2] = 24;
     params.reverse[0] = false; params.reverse[1] = true; params.reverse[2] = true;
-    // (min, max) = (40, 150) sensor range due to terabee sensor characteristics
+    params.activeState[0] = LOW; params.activeState[1] = LOW; params.activeState[2] = LOW;
     params.sensor_range[0] = 50; params.sensor_range[1] = 150; params.sensor_range[2] = 100; 
     params.default_value[0] = 30; params.default_value[1] = 90; params.default_value[2] = 30;  
-    // lowest possible is 0, highest possible is 127
     params.min_value[0] = 0; params.min_value[1] = 50; params.min_value[2] = 90;
     params.max_value[0] = 127; params.max_value[1] = 100; params.max_value[2] = 127;
+    params.activePreset = 2;
+    params.presetState[0] = HIGH; params.presetState[1] = LOW; params.presetState[2] = HIGH;
 }
 
 void initialise_preset3() {
     params.cc_num[0] = 22;  params.cc_num[1] = 23; params.cc_num[2] = 24;
     params.reverse[0] = false; params.reverse[1] = false; params.reverse[2] = false;
+    params.activeState[0] = HIGH; params.activeState[1] = HIGH; params.activeState[2] = LOW;
     params.sensor_range[0] = 150; params.sensor_range[1] = 150; params.sensor_range[2] = 150; 
     params.default_value[0] = 64; params.default_value[1] = 64; params.default_value[2] = 64;  
     params.min_value[0] = 0; params.min_value[1] = 0; params.min_value[2] = 0;
     params.max_value[0] = 127; params.max_value[1] = 127; params.max_value[2] = 127;
+    params.activePreset = 3;
+    params.presetState[0] = HIGH; params.presetState[1] = HIGH; params.presetState[2] = LOW;
+}
+
+void Upload_preset_state()  {
+  if(params.activePreset == 1)
+  {
+      initialise_preset1();
+  }
+  else if(params.activePreset == 2)
+  {
+      initialise_preset2();
+  }
+  else if(params.activePreset == 3)
+  {
+      initialise_preset3();
+  }
+}
+
+// TO DEBUG
+void HoldModeButtons(int r3, int r4, int r5)
+{   
+  // PRESET BUTTON 1
+  buttonPress(r3, lastButtonState3, params.activeState[0], ledPin3,
+                buttonState3, lastDebounceTime3, debounceDelay3);
+
+  // PRESET BUTTON 2
+  buttonPress(r4, lastButtonState4, params.activeState[1], ledPin4,
+                buttonState4, lastDebounceTime4, debounceDelay4);
+
+  // PRESET BUTTON 3
+  buttonPress(r5, lastButtonState5, params.activeState[2], ledPin5,
+                buttonState5, lastDebounceTime5, debounceDelay5);  
 }
